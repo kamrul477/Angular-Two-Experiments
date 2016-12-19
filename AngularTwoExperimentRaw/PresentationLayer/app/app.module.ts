795dc2227f4a34ed6340588eac1e09a5e07b11dc
User <User@DESKTOP-6L4MMRG>
2016-12-19 15:18:15 +0600

    1st commit

---
+++ AngularTwoExperimentRaw/PresentationLayer/app/app.module.ts
@@ -0,0 +1,17 @@
+//the main entry point of the angular appliction
+//main=>appModule=>aooComponent
+//here we reference all the component globally to viwed from all over the application.
+
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }  from './app.component';
+import { ProductListComponent } from './product/product-list.component';
+
+
+@NgModule({
+  imports: [ BrowserModule ],
+  declarations: [AppComponent, ProductListComponent ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
---
+++ AngularTwoExperimentRaw/PresentationLayer/app/product/product-list.component.ts
@@ -0,0 +1,8 @@
+﻿import {Component} from '@angular/core';
+
+@Component({
+    selector: 'pm-products',
+    templateUrl:'app/product/product-list.component.html'
+})
+
+export class ProductListComponent{ }
