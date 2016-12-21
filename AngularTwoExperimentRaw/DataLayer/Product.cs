d2bfd6ba4f3c5c94c84e408db64759f6ace5fe67
User <User@DESKTOP-6L4MMRG>
2016-12-21 17:25:30 +0600

    api added

---
+++ AngularTwoExperimentRaw/DataLayer/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductCode { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+        //"productId": 1,
+        //"productName": "Leaf Rake",
+        //"productCode": "GDN-0011",
+        //"releaseDate": "March 19, 2016",
+        //"description": "Leaf rake with 48-inch wooden handle.",
+        //"price": 19.95,
+        //"starRating": 3.2,
+        //"imageUrl": "http:/
+
+
+    }
+}
